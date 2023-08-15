592e9d396a4ac2ec09b7bd8cf6a6ac87b7d97fa8
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-08-15 12:41:06 +0300

    commit

--- chainlit/model.py
+++ chainlit/model.py
@@ -60,3 +60,14 @@ def final_result(query):
     return response
 
 
+# Chainlit ######
+@cl.on_chat_start
+async def start():
+    chain = qa_bot()
+    msg = cl.Message(content='Starting the bot....')
+    await msg.send()
+    msg.content = "Hi, Welcome to the MedReview Bot. What is your query?"
+    await msg.update()
+    cl.user_session.set("chain", chain)
+    
+    

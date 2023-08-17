c88e27e6385196fb788894d3695d67b698b0c49e
Ian Bravyn <ianbravynsa@gmail.com>
2023-08-17 09:44:43 +0300

    .

--- frontend/sayansi/src/app/components/DashboardItems.jsx
+++ frontend/sayansi/src/app/components/DashboardItems.jsx
@@ -1,9 +1,11 @@
 import Stack from 'react-bootstrap/Stack';
 import { Button } from 'react-bootstrap';
+import Logo from '../components/Logo'
 
 function DashboardItems() {
   return (
     <Stack gap={3}>
+        <Logo/>
      <Button variant='secondary'>Upload Course Outline</Button>
      <Button variant='secondary'>Generate Course Content</Button>
      <Button variant='secondary'>Evaluate Course Content</Button>

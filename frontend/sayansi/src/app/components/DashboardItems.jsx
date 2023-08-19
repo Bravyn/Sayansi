6c8ded2d2a67abc963a7a37612e5f8ff9bcf68ec
Ian Bravyn <ianbravynsa@gmail.com>
2023-08-19 03:55:12 +0300

    Update DashboardItems.jsx

--- frontend/sayansi/src/app/components/DashboardItems.jsx
+++ frontend/sayansi/src/app/components/DashboardItems.jsx
@@ -1,9 +1,9 @@
 import Stack from 'react-bootstrap/Stack';
 import { Button } from 'react-bootstrap';
 import Logo from '../components/Logo'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-function DashboardItems() {
+const DashboardItems = () => {
   const [activeWin, setActiveWin] = useState(0)
 
   const handleButton = (btn_number) => {
@@ -12,10 +12,11 @@ function DashboardItems() {
 
   return (
     <Stack gap={3}>
-        <Logo/>
-     <Button variant='secondary'>Upload Course Outline</Button>
-     <Button variant='secondary'>Generate Course Content</Button>
-     <Button variant='secondary'>Evaluate Course Content</Button>
+        {/*<Logo/>*/}
+     <Button variant= {activeWin === 1 ? 'primary' : 'secondary'}
+ onClick={() => handleButton(1)}>Upload Course Outline</Button>
+     <Button variant= {activeWin === 2 ? 'primary' : 'secondary'} onClick={() => handleButton(2)}>Generate Course Content</Button>
+     <Button variant={activeWin === 3 ? 'primary' : 'secondary'} onClick={() => handleButton(3)}>Evaluate Course Content</Button>
     </Stack>
   );
 }
